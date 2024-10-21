82c581057185f80d4d229053199ef0c4bbaad068
Avinash <avinashkumarofficial95@gmail.com>
2024-10-21 11:22:27 +0530

    vite setup for shopping cart

---
+++ React-3/shopping-cart/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import InputBox from './Component/InputBox'
+
+function App() {
+    return (
+    <>
+      <div>Shopping cart</div>
+      <InputBox></InputBox>
+    </>
+  )
+}
+
+export default App
