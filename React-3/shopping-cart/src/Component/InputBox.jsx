398e306c1eea73bad39c036cb26d8a70dc361ae9
Avinash <avinashkumarofficial95@gmail.com>
2024-10-21 12:48:12 +0530

    onChange event practised

--- React-3/shopping-cart/src/Component/InputBox.jsx
+++ React-3/shopping-cart/src/Component/InputBox.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
 function InputBox() {
+  const [content,setContent] = React.useState('');
+  function handleClick(){
+    setContent('');
+  }
+  function handleChange(e){
+    const updatedValue=e.target.value;
+    setContent(updatedValue);
+  }
   return (
-    <div>InputBox</div>
+    <>
+      <input type='text' value={content} onChange={handleChange}/>
+      <button onClick={handleClick}>Alert Change</button>
+    </>
+    
   )
 }
 
